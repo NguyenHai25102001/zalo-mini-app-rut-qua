f1084f08923928b541207b0ae6244c0e142c4d67
edohajime <nvh25102001@gmail.com>
2024-10-05 11:40:10 +0700

    Refactor code to handle copying link for gift relatives

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -159,6 +159,8 @@ const HomePage: React.FunctionComponent = () => {
       const res = await axios.get(api.checkWithdrawGift(), {
         params: { phone: phoneUser },
       });
+      console.log(res.data);
+
 
       setCheckWithdraw(res.data);
 
@@ -280,6 +282,43 @@ const HomePage: React.FunctionComponent = () => {
     }
   };
 
+  const handleCopyLink = () => {
+    console.log(window.location.href);
+
+    const baseUrl = window.location.href.split('/gift')[0];
+
+    if (!checkWithdraw?.info_withdraw_relative?.phone || !checkWithdraw?.info_withdraw.code_invoice_kiotviet) {
+      console.log(checkWithdraw?.info_withdraw_relative?.phone, checkWithdraw?.info_withdraw.code_invoice_kiotviet);
+
+      openSnackbar({
+        type: "error",
+        text: "Đã xảy ra lỗi khi sao chép link",
+        position: "top",
+        duration: 3000,
+      });
+      return;
+    }
+    const link = `https://zalo.me/s/1017356468012563396/gift-relatives/?env=DEVELOPMENT&version=zdev-13f37aa1?code=${checkWithdraw.info_withdraw.code_invoice_kiotviet}&phone=${checkWithdraw.info_withdraw_relative.phone}`;
+    navigator.clipboard.writeText(link)
+      .then(() => {
+        openSnackbar({
+          type: "success",
+          text: "Link đã được sao chép vào clipboard",
+          position: "top",
+          duration: 3000,
+        });
+      })
+      .catch((error) => {
+        console.error("Error copying link: ", error);
+        openSnackbar({
+          type: "error",
+          text: "Đã xảy ra lỗi khi sao chép link",
+          position: "top",
+          duration: 3000,
+        });
+      });
+  };
+
 
   return (
     <Page className="page bg-main relative ">
@@ -311,20 +350,63 @@ const HomePage: React.FunctionComponent = () => {
       {
         statusGiftUser === 2 && (
           <>
-            <div className="absolute top-[50%] left-0 w-full -translate-y-[50%] overflow-auto p-4">
-              <div className="text-center flex justify-center">
-                <img src={images.box_gift} alt="" className="shake" />
+
+            <>
+              <div className="absolute top-[50%] left-0 w-full -translate-y-[50%] overflow-auto p-4">
+                <div className="text-center flex justify-center">
+                  <img src={images.box_gift} alt="" className="shake" onClick={handleCopyLink} />
+                </div>
+                <p className="element" style={{ fontFamily: 'SVN-KongaPro' }}>Nhấn vào hộp quà để sao chép link gửi cho người thân</p>
+                <p className="text-center element text-4xl w-full font-semibold" style={{ fontFamily: 'SVN-KongaPro' }}>
+                  Đang chờ người thân
+                  <br />
+                  của bạn rút quà...</p>
+
               </div>
-              <p className="text-center element text-4xl w-full font-semibold" style={{ fontFamily: 'SVN-KongaPro' }}>
-                Đang chờ người thân
-                <br />
-                của bạn rút quà...</p>
 
-            </div>
+              <div className="absolute bottom-6 left-0 w-full flex justify-center flex-col">
+                {/* <div className="flex justify-center mb-2">
+                  <div
+                    className="py-3 px-14 rounded text-white text-center cursor-pointer   font-bold text-xl"
+                    style={{
+                      background: 'linear-gradient(357.61deg, #FC670A 1.94%, #FFF16A 94.52%)',
+                      fontFamily: 'SVN-KongaPro',
+                    }}
+                    onClick={handleCopyLink}
+                  >
+                    Sao chép link
+                  </div>
+                </div> */}
+                <img src={images.btn_lam_moi} alt="" className=" bject-contain" onClick={checkWithdrawGift} />
 
-            <div className="absolute bottom-6 left-0 w-full flex justify-center ">
-              <img src={images.btn_lam_moi} alt="" className=" bject-contain" onClick={checkWithdrawGift} />
-            </div>
+              </div>
+
+            </>
+
+
+            {/* {
+              checkWithdraw?.info_withdraw.type === 0 && checkWithdraw.info_withdraw_relative && (
+                <div className="">
+                  <div className="absolute top-[55%] -translate-y-[50%] left-[50%] -translate-x-[50%] w-full flex justify-center px-5">
+                    <div className="bg-[#C70D52] bg-opacity-35 w-full rounded py-10 px-5">
+                      <p className='text-blue-950 text-xl font-semibold underline'>Link gửi cho người thân</p>
+                      <div className="flex justify-center mt-5">
+                        <div
+                          className="py-3 p-2 rounded text-white text-center cursor-pointer text-xl font-semibold"
+                          style={{
+                            background: 'linear-gradient(357.61deg, #FC670A 1.94%, #FFF16A 94.52%)'
+                          }}
+                          onClick={handleCopyLink}
+                        >
+                          Sao chép link
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              )
+            } */}
 
           </>
         )
@@ -356,9 +438,12 @@ const HomePage: React.FunctionComponent = () => {
             }
 
 
+
           </>
         )
       }
+
+
 
 
       <ZMPSheet
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,14 +1,30 @@
 export interface CheckWithdrawal {
     status: number;
     type: number;
-    gift: {
+    info_withdraw: {
         id: number;
         id_kiotviet_product: number;
-        code_kiotviet_product: string;
+        code_invoice_kiotviet: string;
         name: string;
         image: string;
         total: number;
         status: number;
+        type: number;
 
+    }
+    info_withdraw_relative?: {
+        id: number,
+        user_id: number,
+        branch_id: 1000000478,
+        type_withdraw: number,
+        phone: string,
+        code_invoice_kiotviet: string,
+        sold_by_name: string,
+        customer_name: string,
+        status_value: string,
+        description: string,
+        payments_day: Date,
+        type: number,
+        status: number,
     }
 }
